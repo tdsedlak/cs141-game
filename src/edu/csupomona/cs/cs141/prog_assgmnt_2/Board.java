4a1d2de17f7390e37e2e1002a8e5eaba8e7b6849
urmom7 <avila.shane@yahoo.com>
2015-05-10 16:08:06 -0700

    Readding Classes
    A few classes got deleted, not sure why, but i’m implementing them back.

---
+++ src/edu/csupomona/cs/cs141/prog_assgmnt_2/Board.java
@@ -0,0 +1,20 @@
+/**
+ * CS 141: Introduction to Programming and Problem Solving
+ * Professor: Edwin Rodr&iacute;guez
+ *
+ * Programming Assignment #1
+ *
+ * <Ninja Assassin Game. Collect the document and escape the
+ * building before getting caught to win.>
+ *
+ * Team SyntaxErrors
+ *   <Shane Avila, Celine Castillo, Andrew Russo, Tom>
+ */
+package edu.csupomona.cs.cs141.prog_assgmnt_2;
+
+/**
+ *
+ */
+public class Board {
+
+}
